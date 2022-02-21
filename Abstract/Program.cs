bb89df16f357aaabc8fbfa320bcbed18449cc0bc
user <user@DESKTOP-3G4HHUT>
2022-02-21 03:48:18 -0800

    Add project files.

---
+++ Abstract/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Abstract
+{
+    abstract class Rectangle
+    {
+        public int a;
+        public int b;
+        public abstract void area(int a, int b);
+        public void parameter(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+            Console.WriteLine(2 * this.a * this.b);
+        }
+    }
+    class Squre : Rectangle
+    {
+        public override void area(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+            Console.WriteLine(this.a * this.b);
+
+        }
+    }
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Squre obj = new Squre();
+            obj.parameter(10, 20);
+            obj.area(20, 30);
+        }
+    }
+}
